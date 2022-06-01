dbf8e2b078ab1305e642dbead33ac06e63f557ff
AJenkinsSoultions <ajenkinsuiw@gmail.com>
2022-06-01 07:39:07 -0500

    Add exit button

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from tkinter import *
-
+from tkinter import filedialog as fd
 
 root = Tk()
 root.title('Root Window')
 root.geometry('600x400+50+50')
 
-
-
+button_exit = Button(root, text='EXIT', command=exit)
+button_exit.grid(row=0, column=0, padx=10, pady=10)
 
 root.mainloop()
